fce705db66edcad56c7869580bb8fa111a0bb016
Laura Lujan <laura.lujan@EDCMXENLT0467.local>
2021-05-07 12:03:11 -0500

    Mod image loading

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
       <main className=" container mx-auto max-w-2xl ">
         <div className=" m-auto mx-auto">
-          <Image src="/contacts.png" priority={true} width={900} height={600} alt="contacting ilustration"/>
+          <Image src="/contacts.png" loading="eager" width={900} height={600} alt="contacting ilustration"/>
         </div>
         <div className="mx-auto m-auto">
           <button className="block bg-red-400 hover:bg-red-500 text-white text-lg mx-auto p-4 rounded-full" onClick={() => onClick()}>
