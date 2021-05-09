a0a32f14602c4bb2593804d7b2a3c58fd35d616d
Laura Lujan <laura.lujan@EDCMXENLT0467.local>
2021-05-09 12:41:45 -0500

    Add on key down event to search bar

--- components/searchBar/SearchBar.jsx
+++ components/searchBar/SearchBar.jsx
@@ -1,35 +1,46 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
 
-
 const SearchBar = (props) => {
   const router = useRouter();
   const [queryName, setQueryName] = useState("");
   const [invisible, setInvisible] = useState(true);
-  
-  const btnInvisible = "invisible px-6 py-3 mt-3 text- text-white transition-all duration-150 ease-linear rounded-full shadow outline-none bg-red-400 hover:bg-red-500 hover:shadow-lg focus:outline-none ";
-  const btnVisible = "px-6 py-3 my-2 mx-2 text- text-white transition-all duration-150 ease-linear rounded-full shadow outline-none bg-red-400 hover:bg-red-500 hover:shadow-lg focus:outline-none ";
+
+  const btnInvisible =
+    "invisible px-6 py-3 mt-3 text- text-white transition-all duration-150 ease-linear rounded-full shadow outline-none bg-red-400 hover:bg-red-500 hover:shadow-lg focus:outline-none ";
+  const btnVisible =
+    "px-6 py-3 my-2 mx-2 text- text-white transition-all duration-150 ease-linear rounded-full shadow outline-none bg-red-400 hover:bg-red-500 hover:shadow-lg focus:outline-none ";
   const onClick = (queryName) => {
     props.searchContact(queryName);
     setInvisible(false);
     setQueryName("");
-    
   };
 
   const onClickBack = () => {
-    props.loadContacts()
-    setInvisible(true)
+    props.loadContacts();
+    setInvisible(true);
+  };
 
-  }
+  const onKeyDown = (event) => {
+    if (event.key === "Enter") {
+      props.searchContact(queryName);
+      setInvisible(false);
+      setQueryName("");
+    }
+  };
 
   return (
     <div className="flex">
-      <div>{
-        invisible ? <button className={btnInvisible}>Back</button> : <button className={btnVisible} onClick={() => onClickBack()}>Back</button>
-        }
-        </div>
+      <div>
+        {invisible ? (
+          <button className={btnInvisible}>Back</button>
+        ) : (
+          <button className={btnVisible} onClick={() => onClickBack()}>
+            Back
+          </button>
+        )}
+      </div>
       <div className="bg-white flex items-center rounded-full shadow-lg h-16 max-w-lg ">
-        
         <input
           className="h-auto rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none "
           id="search"
@@ -37,6 +48,7 @@ const SearchBar = (props) => {
           placeholder="Search"
           value={queryName}
           onChange={(e) => setQueryName(e.target.value)}
+          onKeyDown={onKeyDown}
         />
 
         <div className="p-4">
